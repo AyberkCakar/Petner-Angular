762e8a79104fbab068918f16358978bf9c4e16f0
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-24 15:36:37 +0300

    refactor: serve.js

--- serve.js
+++ serve.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/petner-angular'));
+app.use(express.static(__dirname + '/dist/Petner-Angular'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/petner-angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Petner-Angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8020);
