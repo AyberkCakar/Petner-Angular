147026bb5590ceb2d06644820913af20d700ca66
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-27 11:43:17 +0300

    feat: added login alert component

---
+++ src/app/error.model.ts
@@ -0,0 +1,4 @@
+export class ErrorModel {
+  error: boolean;
+  message: string;
+}
--- src/app/user-pages/login/login.component.ts
+++ src/app/user-pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {UserModel} from '../user.model'
 import { AuthService } from '../../../utils/services';
 import { Router } from '@angular/router';
+import {ErrorModel} from '../../error.model'
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router, private _authService: AuthService) { }
   model: UserModel = new UserModel();
-
+  error: ErrorModel = new ErrorModel();
   ngOnInit() {
   }
 
@@ -20,17 +21,18 @@ export class LoginComponent implements OnInit {
   {
     this.model.personEmail=email;
     this.model.password=password;
-
     try{
       let response = await this._authService.login(this.model);
-      if(response['message'] =='Giriş Başarılı')
+      this.error.error=response['error'];
+      this.error.message= response['message'];
+      if(this.error.message == 'Giriş Başarılı')
       {
         this.router.navigateByUrl('/dashboard');
       }
     }catch(e)
     {
-      console.log(e);
-      debugger;
+      this.error.error=true;
+      this.error.message= '* Kullanıcı veya Şifre hatalı !';
     }
   }
 }
