434844da386805eca821d121bb8797c25488ce0e
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-19 21:23:59 +0300

    feat: added add advert page agm maps component

--- src/app/add-advert/add-advert.component.ts
+++ src/app/add-advert/add-advert.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,NgZone,ViewChild, ElementRef, } from '@angular/core';
 import { ImageService, AdvertService } from '../../utils/services';
 import { AdvertModel, Animal, Address } from './advert.model';
 import { NotifierService } from 'angular-notifier';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
+
 class ImageSnippet {
   pending: boolean = false;
   status: string = 'init';
@@ -20,11 +22,23 @@ export class AddAdvertComponent implements OnInit {
   adressModel: Address = new Address();
   selectedFile: ImageSnippet;
   image: string;
+
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
+
+  @ViewChild('search',{static: false})
+  public searchElementRef: ElementRef;
+
   constructor(
     private imageService: ImageService,
     private AdvertService: AdvertService,
     private notifier: NotifierService,
-    private router: Router
+    private router: Router,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
   ) { }
 
   public showNotification(type: string, message: string): void {
@@ -66,8 +80,8 @@ export class AddAdvertComponent implements OnInit {
     this.adressModel.district = district;
     this.adressModel.fullAddress = adress;
     this.adressModel.province = city;
-    this.adressModel.latitude = 41.458;
-    this.adressModel.longitude = 34.7548;
+    this.adressModel.latitude = this.latitude;
+    this.adressModel.longitude = this.longitude;
     this.advertModel.advertisementAddress = this.adressModel;
 
     try {
@@ -80,7 +94,50 @@ export class AddAdvertComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
+ 
+  ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
   }
 
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+
+  markerDragEnd($event: MouseEvent) {
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+        }
+      }
+    });
+  }
 }
