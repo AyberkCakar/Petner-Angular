e3706656b6c1bd1de36c39fbf0d976ea3bfb3422
AyberkCakar <ayberkcakar35@gmail.com>
2021-01-02 18:42:51 +0300

    feat: added Password strength

--- src/app/user-pages/register/register.component.ts
+++ src/app/user-pages/register/register.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Inject,Renderer2 } from '@angular/core';
 import {UserModel} from '../user.model'
 import { AuthService } from '../../../utils/services';
 import { Router } from '@angular/router';
 import {ErrorModel} from '../../error.model'
-
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -12,12 +12,112 @@ import {ErrorModel} from '../../error.model'
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router, private _authService: AuthService) { }
+  constructor(private router: Router, private _authService: AuthService,
+    private _renderer2: Renderer2, 
+    @Inject(DOCUMENT) private _document: Document
+    ) { 
+    
+  }
   model: UserModel = new UserModel();
   error: ErrorModel = new ErrorModel();
+  public ngOnInit() {
 
-  ngOnInit() {
-  }
+    let script = this._renderer2.createElement('script');
+    
+    script.type = `text/javascript`;
+    script.text = `
+        {
+          $(document).ready(function() {
+            $('#password').keyup(function() {
+                var password = $('#password').val();
+                if (checkStrength(password) == 'Strong') {
+                    $('#sign-up').attr('disabled', false);
+                }
+                else{
+                  $('#sign-up').attr('disabled', true);
+                }
+            });
+    
+            function checkStrength(password) {
+                var strength = 0;
+    
+                    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+                    strength += 1;
+                    $('.low-upper-case').addClass('text-success');
+                    $('.low-upper-case i').removeClass('fa-file-text').addClass('fa-check');
+                    $('#popover-password-top').addClass('hide');
+                } else {
+                    $('.low-upper-case').removeClass('text-success');
+                    $('.low-upper-case i').addClass('fa-file-text').removeClass('fa-check');
+                    $('#popover-password-top').removeClass('hide');
+                }
+    
+                if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
+                    strength += 1;
+                    $('.one-number').addClass('text-success');
+                    $('.one-number i').removeClass('fa-file-text').addClass('fa-check');
+                    $('#popover-password-top').addClass('hide');
+    
+                } else {
+                    $('.one-number').removeClass('text-success');
+                    $('.one-number i').addClass('fa-file-text').removeClass('fa-check');
+                    $('#popover-password-top').removeClass('hide');
+                }
+    
+                if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+                    strength += 1;
+                    $('.one-special-char').addClass('text-success');
+                    $('.one-special-char i').removeClass('fa-file-text').addClass('fa-check');
+                    $('#popover-password-top').addClass('hide');
+    
+                } else {
+                    $('.one-special-char').removeClass('text-success');
+                    $('.one-special-char i').addClass('fa-file-text').removeClass('fa-check');
+                    $('#popover-password-top').removeClass('hide');
+                }
+    
+                if (password.length > 7) {
+                    strength += 1;
+                    $('.eight-character').addClass('text-success');
+                    $('.eight-character i').removeClass('fa-file-text').addClass('fa-check');
+                    $('#popover-password-top').addClass('hide');
+    
+                } else {
+                    $('.eight-character').removeClass('text-success');
+                    $('.eight-character i').addClass('fa-file-text').removeClass('fa-check');
+                    $('#popover-password-top').removeClass('hide');
+                }
+        
+                if (strength < 2) {
+                    $('#result').removeClass()
+                    $('#password-strength').addClass('progress-bar-danger');
+    
+                    $('#result').addClass('text-danger').text('Very Week');
+                    $('#password-strength').css('width', '10%');
+                } else if (strength == 2) {
+                    $('#result').addClass('good');
+                    $('#password-strength').removeClass('progress-bar-danger');
+                    $('#password-strength').addClass('progress-bar-warning');
+                    $('#result').addClass('text-warning').text('Week')
+                    $('#password-strength').css('width', '60%');
+                    return 'Week'
+                } else if (strength == 4) {
+                    $('#result').removeClass()
+                    $('#result').addClass('strong');
+                    $('#password-strength').removeClass('progress-bar-warning');
+                    $('#password-strength').addClass('progress-bar-success');
+                    $('#result').addClass('text-success').text('Strength');
+                    $('#password-strength').css('width', '100%');
+
+                    return 'Strong'
+                }
+            }
+        });
+        }
+    `;
+
+    this._renderer2.appendChild(this._document.body, script);
+}
 
   async onRegister(name: string, surname: string, number: string, email: string, password: string)
   {
