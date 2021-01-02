a6949ed257e8748d3c9bd28dd9fd4582dcf6a597
DorukanE <dorukaneskiceri@gmail.com>
2021-01-02 21:30:46 +0300

    feat: added password change to update user page

--- src/app/update-account/update-account.component.ts
+++ src/app/update-account/update-account.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject, Renderer2 } from '@angular/core';
 import {NotifierService} from 'angular-notifier';
 import { UserModel } from '../user-pages/user.model';
 import { AuthService } from 'src/utils/services';
 import { UpdatePasswordModel } from './update-password-model.model';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-update-account',
@@ -12,14 +13,23 @@ import { UpdatePasswordModel } from './update-password-model.model';
 export class UpdateAccountComponent implements OnInit {
   model: UserModel = new UserModel();
   pswModel: UpdatePasswordModel = new UpdatePasswordModel();
+  passwordConfirmation;
 
   constructor(
     private notifier: NotifierService,
-    private authService: AuthService
+    private authService: AuthService,
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
   ) { }
 
   async ngOnInit() {
-    this.model = <UserModel>await this.authService.getAccountAsync()
+    this.model = <UserModel>await this.authService.getAccountAsync();
+    let script = this._renderer2.createElement('script');
+
+    script.type = `text/javascript`;
+    script.src = `../../assets/script/password.js`;
+
+    this._renderer2.appendChild(this._document.body, script);
   }
 
   public showNotification( type: string, message: string ): void {
@@ -38,12 +48,25 @@ export class UpdateAccountComponent implements OnInit {
     }
   }
 
-  async onUpdatePsw(newPassword: string,oldPassword: string){
+  async onUpdatePsw(newPassword: string,passwordConfirmation:string,oldPassword: string){
     try {
       this.pswModel.newPassword=newPassword;
       this.pswModel.oldPassword=oldPassword;
-      let response =await this.authService.updatePasswordAsync(this.pswModel);
-      this.showNotification( 'success', response['message'] );
+      if(oldPassword != "")
+      {
+        if(passwordConfirmation == newPassword)
+        {
+          let response =await this.authService.updatePasswordAsync(this.pswModel);
+          this.showNotification( 'success', response['message'] );
+        }
+        else{
+          this.showNotification( 'error','* Girilen Şifreler Eşleşmiyor!');      
+        }
+      }
+      else{
+        this.showNotification( 'error','* Eski Şifrenizi Giriniz!');      
+      }
+    
     } catch (error) {
       this.showNotification( 'error', error.message );      
     }
--- src/app/user-pages/register/register.component.ts
+++ src/app/user-pages/register/register.component.ts
@@ -4,7 +4,6 @@ import { AuthService } from '../../../utils/services';
 import { Router } from '@angular/router';
 import { ErrorModel } from '../../error.model'
 import { DOCUMENT } from '@angular/common';
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -25,101 +24,7 @@ export class RegisterComponent implements OnInit {
     let script = this._renderer2.createElement('script');
 
     script.type = `text/javascript`;
-    script.text = `
-        {
-          function togglePassword() {
-          var element = document.getElementById("password");
-          element.type = element.type == "password" ? "text" : "password";
-          }
-          
-          $(document).ready(function() {
-            $('#password').keyup(function() {
-                var password = $('#password').val();
-                if (checkStrength(password) == 'Strong') {
-                    $('#sign-up').attr('disabled', false);
-                }
-                else{
-                  $('#sign-up').attr('disabled', true);
-                }
-            });
-    
-            function checkStrength(password) {
-                var strength = 0;
-    
-                    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
-                    strength += 1;
-                    $('.low-upper-case').addClass('text-success');
-                    $('.low-upper-case i').removeClass('fa-file-text').addClass('fa-check');
-                    $('#popover-password-top').addClass('hide');
-                } else {
-                    $('.low-upper-case').removeClass('text-success');
-                    $('.low-upper-case i').addClass('fa-file-text').removeClass('fa-check');
-                    $('#popover-password-top').removeClass('hide');
-                }
-    
-                if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
-                    strength += 1;
-                    $('.one-number').addClass('text-success');
-                    $('.one-number i').removeClass('fa-file-text').addClass('fa-check');
-                    $('#popover-password-top').addClass('hide');
-    
-                } else {
-                    $('.one-number').removeClass('text-success');
-                    $('.one-number i').addClass('fa-file-text').removeClass('fa-check');
-                    $('#popover-password-top').removeClass('hide');
-                }
-    
-                if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
-                    strength += 1;
-                    $('.one-special-char').addClass('text-success');
-                    $('.one-special-char i').removeClass('fa-file-text').addClass('fa-check');
-                    $('#popover-password-top').addClass('hide');
-    
-                } else {
-                    $('.one-special-char').removeClass('text-success');
-                    $('.one-special-char i').addClass('fa-file-text').removeClass('fa-check');
-                    $('#popover-password-top').removeClass('hide');
-                }
-    
-                if (password.length > 7) {
-                    strength += 1;
-                    $('.eight-character').addClass('text-success');
-                    $('.eight-character i').removeClass('fa-file-text').addClass('fa-check');
-                    $('#popover-password-top').addClass('hide');
-    
-                } else {
-                    $('.eight-character').removeClass('text-success');
-                    $('.eight-character i').addClass('fa-file-text').removeClass('fa-check');
-                    $('#popover-password-top').removeClass('hide');
-                }
-        
-                if (strength < 2) {
-                    $('#result').removeClass()
-                    $('#password-strength').addClass('progress-bar-danger');
-    
-                    $('#result').addClass('text-danger').text('Very Week');
-                    $('#password-strength').css('width', '10%');
-                } else if (strength == 2) {
-                    $('#result').addClass('good');
-                    $('#password-strength').removeClass('progress-bar-danger');
-                    $('#password-strength').addClass('progress-bar-warning');
-                    $('#result').addClass('text-warning').text('Week')
-                    $('#password-strength').css('width', '60%');
-                    return 'Week'
-                } else if (strength == 4) {
-                    $('#result').removeClass()
-                    $('#result').addClass('strong');
-                    $('#password-strength').removeClass('progress-bar-warning');
-                    $('#password-strength').addClass('progress-bar-success');
-                    $('#result').addClass('text-success').text('Strength');
-                    $('#password-strength').css('width', '100%');
-
-                    return 'Strong'
-                }
-            }
-        });
-        }
-    `;
+    script.src = `../../assets/script/password.js`;
 
     this._renderer2.appendChild(this._document.body, script);
   }
